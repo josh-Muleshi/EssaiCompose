27270758e64fe9fab1c88edfd1b18d4973e60c55
josh muleshi <jmuleshi2@gmail.com>
2021-08-19 14:20:40 +0100

    textView, modifiers and alignment

--- app/src/main/java/com/example/essai/MainActivity.kt
+++ app/src/main/java/com/example/essai/MainActivity.kt
@@ -3,36 +3,83 @@ package com.example.essai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.essai.ui.theme.EssaiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EssaiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Column(
+                modifier = Modifier
+                    .background(color = Color.LightGray)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Greeting("josh")
+                Greeting("josue")
+                Greeting("yoshua")
             }
         }
     }
 }
 
+//@Composable
+//fun MyApp(content: @Composable () -> Unit) {
+//    EssaiTheme {
+//        Surface(color = MaterialTheme.colors.background) {
+//            content()
+//        }
+//    }
+//}
+
+
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text (
+        text = "Hello $name!",
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Cyan,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .border(2.dp, color = Color.Cyan)
+            .padding(24.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    EssaiTheme {
+    Column(
+        modifier = Modifier
+            .background(color = Color.LightGray)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Greeting("josh")
+        Greeting("josue")
+        Greeting("yoshua")
     }
 }
