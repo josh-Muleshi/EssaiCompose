b01be1d906673456298864332d96b7e00333a055
josh muleshi <jmuleshi2@gmail.com>
2021-08-20 14:25:35 +0100

    next part of the course on google codelabs

--- app/src/main/java/com/example/essai/MainActivity.kt
+++ app/src/main/java/com/example/essai/MainActivity.kt
@@ -7,69 +7,41 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.essai.ui.theme.EssaiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                DemoBottom()
-            }
+            MyApp()
         }
     }
 }
 @Composable
-fun DemoBottom() {
-    val context = LocalContext.current
-
-    Button(onClick = {
-        Toast.makeText(context, "btn clicked", Toast.LENGTH_LONG).show()
-    }) {
-        Text(text = "click me")
-    }
-
-    Button(onClick = {
-        Toast.makeText(context, "btn clicked", Toast.LENGTH_LONG).show()
-        },
-        enabled = false
-        ) {
-        Text(text = "click me")
-    }
-
-    TextButton(onClick = {
-        Toast.makeText(context, "btn clicked", Toast.LENGTH_LONG).show()
-    }) {
-        Text(text = "click me")
-    }
-
-    OutlinedButton(onClick = {
-        Toast.makeText(context, "btn clicked", Toast.LENGTH_LONG).show()
-    }) {
-        Text(text = "click me")
+fun MyApp() {
+    EssaiTheme {
+        Surface(color = Color.Yellow) {
+            Greeting(name = "Android")
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Preview
 @Composable
 fun DefaultPreview() {
-    Column(
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        DemoBottom()
-    }
+    MyApp()
 }
