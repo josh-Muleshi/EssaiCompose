a5b838aa23d0e31cdacf21804dd4265f0b765be3
josh muleshi <jmuleshi2@gmail.com>
2021-08-20 14:13:06 +0100

    learn buttons

--- app/src/main/java/com/example/essai/MainActivity.kt
+++ app/src/main/java/com/example/essai/MainActivity.kt
@@ -1,85 +1,75 @@
 package com.example.essai
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.essai.ui.theme.EssaiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Column(
-                modifier = Modifier
-                    .background(color = Color.LightGray)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
             ) {
-                Greeting("josh")
-                Greeting("josue")
-                Greeting("yoshua")
+                DemoBottom()
             }
         }
     }
 }
-
-//@Composable
-//fun MyApp(content: @Composable () -> Unit) {
-//    EssaiTheme {
-//        Surface(color = MaterialTheme.colors.background) {
-//            content()
-//        }
-//    }
-//}
-
-
-
 @Composable
-fun Greeting(name: String) {
-    Text (
-        text = "Hello $name!",
-        fontSize = 32.sp,
-        fontWeight = FontWeight.Bold,
-        color = Color.Cyan,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .background(color = Color.Yellow)
-            .border(2.dp, color = Color.Cyan)
-            .padding(24.dp)
-    )
+fun DemoBottom() {
+    val context = LocalContext.current
+
+    Button(onClick = {
+        Toast.makeText(context, "btn clicked", Toast.LENGTH_LONG).show()
+    }) {
+        Text(text = "click me")
+    }
+
+    Button(onClick = {
+        Toast.makeText(context, "btn clicked", Toast.LENGTH_LONG).show()
+        },
+        enabled = false
+        ) {
+        Text(text = "click me")
+    }
+
+    TextButton(onClick = {
+        Toast.makeText(context, "btn clicked", Toast.LENGTH_LONG).show()
+    }) {
+        Text(text = "click me")
+    }
+
+    OutlinedButton(onClick = {
+        Toast.makeText(context, "btn clicked", Toast.LENGTH_LONG).show()
+    }) {
+        Text(text = "click me")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Column(
-        modifier = Modifier
-            .background(color = Color.LightGray)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
     ) {
-        Greeting("josh")
-        Greeting("josue")
-        Greeting("yoshua")
+        DemoBottom()
     }
 }
