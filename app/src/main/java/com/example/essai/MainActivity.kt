7535b30aaa9a052b3a55a65816a85a9e99501e06
josh muleshi <jmuleshi2@gmail.com>
2021-08-21 08:06:15 +0100

    Modifier

--- app/src/main/java/com/example/essai/MainActivity.kt
+++ app/src/main/java/com/example/essai/MainActivity.kt
@@ -3,19 +3,21 @@ package com.example.essai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.essai.ui.theme.EssaiTheme
@@ -24,65 +26,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp{
-                MyScreenContent()
+            EssaiTheme {
+                PhotoCard()
             }
         }
     }
 }
 @Composable
-fun MyApp(content: @Composable () -> Unit) {
+fun PhotoCard(modifier: Modifier = Modifier){
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { /* Ignoring onClick */ })
+            .padding(16.dp)
+
+    ) {
+        Surface(
+            modifier
+                .size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+        Column(
+            modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Josh Muleshi", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun PhotoCardPreview(){
     EssaiTheme {
-        Surface(color = Color.Yellow) {
-            content()
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
-}
-@Composable
-fun NameList(names: List<String>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(items = names) { name ->
-            Greeting(name = name)
-            Divider(color = Color.Black)
-        }
-    }
-}
-@Composable
-fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
-    val counterState = remember { mutableStateOf(0) }
-
-    Column(modifier = Modifier.fillMaxHeight()) {
-        NameList(names = names, modifier = Modifier.weight(1f))
-        Counter(
-            count = counterState.value,
-            updateCount = { newCount ->
-                counterState.value = newCount
-            }
-        )
-    }
-}
-
-
-@Composable
-fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    Button(
-        onClick = { updateCount(count+1) },
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = if (count > 5) Color.Green else Color.White
-        )) {
-        Text("I've been clicked $count times")
-    }
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApp{
-        MyScreenContent()
+        PhotoCard()
     }
 }
