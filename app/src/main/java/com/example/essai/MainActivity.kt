c21663c26af30daaf121f3db804a5ad6051bc8a4
josh muleshi <jmuleshi2@gmail.com>
2021-08-25 14:48:50 +0100

    merge project to master

--- app/src/main/java/com/example/essai/MainActivity.kt
+++ app/src/main/java/com/example/essai/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.essai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -20,54 +21,26 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.essai.ui.HomeScreen
 import com.example.essai.ui.theme.EssaiTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EssaiTheme {
-                PhotoCard()
+                HomeScreen()
             }
         }
     }
 }
-@Composable
-fun PhotoCard(modifier: Modifier = Modifier){
-    Row(
-        modifier
-            .padding(8.dp)
-            .clip(RoundedCornerShape(4.dp))
-            .background(MaterialTheme.colors.surface)
-            .clickable(onClick = { /* Ignoring onClick */ })
-            .padding(16.dp)
 
-    ) {
-        Surface(
-            modifier
-                .size(50.dp),
-            shape = CircleShape,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ) {
-            // Image goes here
-        }
-        Column(
-            modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
-        ) {
-            Text(text = "Josh Muleshi", fontWeight = FontWeight.Bold)
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
-            }
-        }
-    }
-
-}
+@ExperimentalFoundationApi
 @Preview(showBackground = true)
 @Composable
-fun PhotoCardPreview(){
+fun myAppPreview(){
     EssaiTheme {
-        PhotoCard()
+        HomeScreen()
     }
 }
